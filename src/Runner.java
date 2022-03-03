4def683f7926ac061377b8df6a7483c3b8506270
Tiago Sales <tgoprince@gmail.com>
2022-03-03 19:09:00 +0100

    Add demo classes

---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+public class Runner {
+
+    public static void main(String[] args) {
+	      Person tiago = new Person("Tiago", 31);
+	      Person davi = new Person("Davi", 1);
+
+        System.out.println(tiago);
+        System.out.println(davi);
+    }
+
+}
